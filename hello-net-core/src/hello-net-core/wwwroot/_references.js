0ef0d217b0d9dccb2d5c699f146233d07c2398bc
sad-whale <sbarikov@gmail.com>
2016-11-06 02:39:18 +0300

    further setup

--- hello-net-core/src/hello-net-core/wwwroot/_references.js
+++ hello-net-core/src/hello-net-core/wwwroot/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <autosync enabled="true" />
+/// <reference path="../gulpfile.js" />
+/// <reference path="_references.min.js" />
 /// <reference path="js/site.js" />
 /// <reference path="lib/angular/angular.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
