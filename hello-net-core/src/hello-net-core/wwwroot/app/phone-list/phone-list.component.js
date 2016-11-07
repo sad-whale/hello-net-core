780b06feb1b363044c7757dc03e05735a885b156
sbari <sbarikov@gmail.com>
2016-11-08 01:41:59 +0300

    step 6 or 7...

--- hello-net-core/src/hello-net-core/wwwroot/app/phone-list/phone-list.component.js
+++ hello-net-core/src/hello-net-core/wwwroot/app/phone-list/phone-list.component.js
@@ -5,18 +5,14 @@ angular.
   module('phoneList').
   component('phoneList', {
       templateUrl: 'app/phone-list/phone-list.template.html',
-      controller: function PhoneListController() {
-          this.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.'
-            }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.'
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.'
-            }
-          ];
-      }
+      controller: ['$http',
+          function PhoneListController($http) {
+              var self = this;
+              self.orderProp = 'age';
+
+              $http.get('app/phones/phones.json').then(function (response) {
+                  self.phones = response.data;
+              });
+          }
+      ]
   });
